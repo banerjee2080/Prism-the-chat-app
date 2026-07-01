7029263c5087e9a136acfa951e06ee4e9812c96d
Amitabha Banerjee <banerjee2110@gmail.com>
2026-07-01 18:50:32 +0530

    some bug fixes and UI changes

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,15 +11,14 @@ dotenv.config();
 
 const PORT = process.env.PORT || 5001;
 
-app.use(express.json());
-app.use(cookieParser());
-
 app.use(
   cors({
     origin: "http://localhost:5173",
     credentials: true,
   }),
 );
+app.use(express.json({ limit: "50mb" }));
+app.use(cookieParser());
 
 
 app.use("/api/auth",authRouter);
